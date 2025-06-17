fc51724da1e1c1d18a03bdf3224f5745d90d6d47
Liang Chen <42459106+liang123993@users.noreply.github.com>
2025-06-17 22:23:41 +1000

    feat: store net value in Player model and API route

--- poker-calc/src/models/Player.ts
+++ poker-calc/src/models/Player.ts
@@ -1,18 +1,16 @@
 import mongoose from "mongoose";
 
-const PlayerSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    buyIn: { type: Number, required: true },
-    cashOut: { type: Number, required: true },
-    net: { type: Number },
-    gameId: { type: mongoose.Schema.Types.ObjectId, ref: "Game" },
-}, {
-        timestamps: true
-});
-
-PlayerSchema.pre("save", function (next) {
-    this.net = this.buyIn - this.cashOut
-    next()
-})
+const PlayerSchema = new mongoose.Schema(
+    {
+        name: { type: String, required: true },
+        buyIn: { type: Number, required: true },
+        cashOut: { type: Number, required: true },
+        net: { type: Number, required: true},
+        gameId: { type: mongoose.Schema.Types.ObjectId, ref: "Game" },
+    },
+    {
+        timestamps: true,
+    }
+);
 
 export default mongoose.models.Player || mongoose.model("Player", PlayerSchema);
